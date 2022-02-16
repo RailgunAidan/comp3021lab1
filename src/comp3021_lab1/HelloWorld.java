044d8897d044c16545ef2e202a844035c432b5c8
aidan <aidan@DESKTOP-5AP21Q9>
2022-02-16 11:23:56 +0800

    commit!!!!

--- src/comp3021_lab1/HelloWorld.java
+++ src/comp3021_lab1/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Commit?");
-		System.out.println("Commit2.?");
+		System.out.println("Commit more");
 	}
 }
